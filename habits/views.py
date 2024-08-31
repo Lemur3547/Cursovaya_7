97a11017d0314e5231cf9c1b9dbc3eaaaa37cbae
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-08-31 16:28:09 +0500

    Написан CRUD для привычек

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
 
 # Create your views here.
+class HabitCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
