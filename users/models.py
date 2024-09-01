891a8223d7002492cbb27de1f4588357e3bf8471
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-09-01 17:05:32 +0500

    Описана модель пользователя

--- users/models.py
+++ users/models.py
@@ -5,4 +5,15 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
