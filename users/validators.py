e40bd6bf82abb8d748da3a4aa1c8756da6c84c92
Emik Ubuntu <emikgal2507@gmail.com>
2024-09-05 22:20:37 +0500

    Обновлен валидатор пользователя

--- users/validators.py
+++ users/validators.py
@@ -6,5 +6,9 @@ class RegistrationValidator:
         pass
 
     def __call__(self, value, *args, **kwargs):
-        if len(value) > 2:
-            raise ValidationError('Введите только email и пароль.')
+        if value.get('is_staff') and value.get('is_superuser'):
+            raise ValidationError('Нельзя использовать поля is_staff и is_superuser.')
+        if value.get('is_staff'):
+            raise ValidationError('Нельзя использовать поле is_staff.')
+        if value.get('is_superuser'):
+            raise ValidationError('Нельзя использовать поле is_superuser.')
